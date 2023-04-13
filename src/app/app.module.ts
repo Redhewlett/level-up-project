872b56a68e008a9250874fa57c022989b0629d8a
teddy edmond <92273094+Redhewlett@users.noreply.github.com>
2023-04-13 12:15:37 +0200

    initial commit, pages and components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { HeaderComponent } from './components/header/header.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
+import { UserStatsComponent } from './components/user-stats/user-stats.component';
+import { AdventureComponent } from './components/adventure/adventure.component';
+import { AdventureInfoComponent } from './components/adventure-info/adventure-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SettingsComponent,
+    HeaderComponent,
+    UserInfoComponent,
+    UserStatsComponent,
+    AdventureComponent,
+    AdventureInfoComponent
   ],
   imports: [
     BrowserModule,
